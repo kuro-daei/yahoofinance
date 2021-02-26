35f649c02a99cc442fc0e6c4e8a22816f274f75a
Eiji Kuroda <eiji-kuroda@dac.co.jp>
2021-02-26 13:56:45 +0000

    bigquery呼べた

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
+const { BigQuery } = require('@google-cloud/bigquery');
+
+const bigquery = new BigQuery();
 
 /**
  * YahooファイナンスのCSVをダウンロードしてくる。
@@ -14,6 +17,23 @@ exports.main = async (req, res) => {
   // eslint-disable-next-line no-undef
   const codes = $(".component-list .col-sm-1_5").
     map((i, item) => $(item).text()).get();
-  console.log(codes.join(','));
+
+  // table 全削除
+  let query = 'DELETE FROM `yahoofinance-data.nikkei400.codes` WHERE true';
+  let options = {
+    query,
+    location: 'asia-northeast1'
+  };
+  await bigquery.createQueryJob(options);
+
+  // codes テーブル データ追加
+  query = 'INSERT `yahoofinance-data.nikkei400.codes` (code) VALUES(';
+  query += codes.join('),(');
+  query += ')';
+  options = {
+    query,
+    location: 'asia-northeast1'
+  };
+  await bigquery.createQueryJob(options);
   res.status(200).send(codes.join(','));
 }
